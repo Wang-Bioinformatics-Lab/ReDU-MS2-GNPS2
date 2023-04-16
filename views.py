7021cfeb4ad48a39e1b479bb1aefec08b6d7367a
Mingxun Wang <mwang87@gmail.com>
2023-04-15 17:49:35 -0700

    adding some configs and views

--- views.py
+++ views.py
@@ -9,10 +9,19 @@ import csv
 import json
 import uuid
 import requests
+import pandas as pd
+import config
 
 @app.route('/', methods=['GET'])
 def renderhomepage():
-    return render_template('homepage.html')
+    metadata_df = pd.read_csv(config.PATH_TO_ORIGINAL_MAPPING_FILE, sep="\t", dtype=str)
+    total_files = len(metadata_df["filename"].unique())
+    
+    return render_template('homepage.html', total_files=total_files, total_identifications=0, total_compounds=0)
+
+@app.route('/metadataselection', methods=['GET'])
+def metadataselection():
+    return render_template('metadataselection.html')
 
 @app.route('/heartbeat', methods=['GET'])
 def testapi():
