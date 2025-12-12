5bb8aff6f5e16e2ca68af46f3437d8ee11a02bb1
Mingxun Wang <mwang87@gmail.com>
2025-12-12 11:01:05 -0800

    reduce the worker count

--- run_server.sh
+++ run_server.sh
@@ -2,6 +2,6 @@
 
 source activate python310
 #python ./main.py
-gunicorn -w 6 --threads=12 --worker-class=gthread -b 0.0.0.0:5000 \
+gunicorn -w 1 --threads=12 --worker-class=gthread -b 0.0.0.0:5000 \
 --timeout 120 --max-requests 100 --max-requests-jitter 100 \
 --graceful-timeout 120 main:app --access-logfile /app/logs/access.log
